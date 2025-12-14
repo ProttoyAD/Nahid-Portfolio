6da88690abd6690aaf9dbf2eb0109a3dbba07590
Prottoy <prottoyadhusher@gmail.com>
2025-12-15 02:35:33 +0600

    upgrade

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,55 +9,55 @@ import { Analytics } from "@vercel/analytics/react";
 import ClientTopProgressBar from "@/components/ClientTopProgressBar";
 
 export const metadata = {
+  title: "Nahid Islam | Portofolio",
+
+  description:
+    "My name is Nahid Islam, I'm a web developer and I'm passionate about it. I'm currently studying at Universitas Negeri Malang.",
+
+  author: "Alvalen Shafelbilyunazra",
+  siteUrl: "https://www.alvalens.my.id",
+  applicationName: "Nahid Islam",
+
+  keywords: [
+    "alvalens",
+    "alvalen",
+    "alvalen shafel",
+    "shafel",
+    "alvalen shafelbilyunazra",
+    "alvalen shafel bilyunazra",
+    "bloodfallen",
+    "alvalen porto",
+    "alvalen um",
+  ],
+
+  openGraph: {
+    type: "website",
+    url: "https://www.alvalens.my.id",
     title: "Nahid Islam | Portofolio",
-
-    description:
-		"My name is Nahid Islam, I'm a web developer and I'm passionate about it. I'm currently studying at Universitas Negeri Malang.",
-
-    author: "Alvalen Shafelbilyunazra",
-    siteUrl: "https://www.alvalens.my.id",
-    applicationName: "Nahid Islam",
-
-    keywords: [
-		"alvalens",
-		"alvalen",
-		"alvalen shafel",
-		"shafel",
-		"alvalen shafelbilyunazra",
-		"alvalen shafel bilyunazra",
-		"bloodfallen",
-		"alvalen porto",
-		"alvalen um",
-	],
-
-    openGraph: {
-		type: "website",
-		url: "https://nahid-islam.vercel.app/",
-		title: "Nahid Islam | Portofolio",
-		site_name: "Nahid Islam | Portofolio",
-		description: "My name is Nahid Islam, This is my portofolio website.",
-		width: 1200,
-		height: 630,
-		images: [
-			{
-				url: "/og-image-rev.png",
-				alt: "Nahid Islam Portofolio",
-			},
-		],
-		site_name: "Nahid Islam | Portofolio",
-	}
+    site_name: "Nahid Islam | Portofolio",
+    description: "My name is Nahid Islam, This is my portofolio website.",
+    width: 1200,
+    height: 630,
+    images: [
+      {
+        url: "/og-image-rev.png",
+        alt: "Nahid Islam Portofolio",
+      },
+    ],
+    site_name: "Nahid Islam | Portofolio",
+  },
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<body suppressHydrationWarning>
-				<ClientTopProgressBar />
-				<Navbar />
-				{children}
-				{/* <Chat /> */}
-				<Analytics />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body>
+        <ClientTopProgressBar />
+        <Navbar />
+        {children}
+        {/* <Chat /> */}
+        <Analytics />
+      </body>
+    </html>
+  );
 }
